8f5ebb9a5e51720366a8ebf02921630a0ed6fc67
keira <keirakist0987@gmail.com>
2024-11-22 13:31:48 +0200

    update

--- script/code.js
+++ script/code.js
@@ -1,13 +1,20 @@
 function calculateBMI() {
-    let weight = document.getElementById("weight").value;
-    let height = document.getElementById("height").value;
+    let weight = +document.getElementById("weight").value;
+    let height = +document.getElementById("height").value;
     let bmi = weight / (height * height); 
 
 
-    document.getElementById("bmi").innerHTML = "Your BMI is: " + bmi;
-    if (bmi < 18.5) {
+    document.getElementById("bmi").innerHTML = "Your BMI is: " + bmi.toFixed(2);
+    if (!weight > 0) {
+      document.getElementById("category").innerHTML = "Please enter a weight/height.";
+    }
+    // if (!height > 0) {
+    //   document.getElementById("category").innerHTML = "Please enter a height.";
+    // }
+    else if (bmi < 18.5) {
       document.getElementById("category").innerHTML = "You are underweight.";
-    } else if (bmi >= 18.5 && bmi < 25) {
+    }
+    else if (bmi >= 18.5 && bmi < 25) {
         document.getElementById("category").innerHTML = "You are normal weight.";
     } else if (bmi >= 25 && bmi < 30) {
       document.getElementById("category").innerHTML = "You are overweight.";
